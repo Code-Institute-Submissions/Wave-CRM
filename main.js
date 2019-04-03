a066e355ffdc26fd1d55d8325fd4a75856894982
Ferenc Kovacs <mr.f.kovacs@gmail.com>
2019-04-03 14:27:53 +0000

    Wave api connected through ajax

--- main.js
+++ main.js
@@ -1,3 +1,60 @@
+function loadDoc() {
+  
+  var xhttp = new XMLHttpRequest();
+  
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var data = JSON.parse(this.responseText);
+      var prettyData = data.data.business.customers.edges.map(transformData);
+      useData(prettyData);
+      
+      console.log(prettyData);
+    }
+  };
+  
+  xhttp.open("POST", "https://gql.waveapps.com/graphql/public", true);
+  xhttp.setRequestHeader('Content-Type', 'application/json');
+  xhttp.setRequestHeader('Accept', 'application/json');
+  xhttp.setRequestHeader('Authorization', 'Bearer 8NBKTN3t9pfYUS0GsEoNapjY6ooRbi');
+  xhttp.send(JSON.stringify({
+              query: `{ 
+                business(id: "QnVzaW5lc3M6NjNiOTVkZGItNWRkOS00MzI0LWEzNGYtMDkxOTJmNjNjNDc0") { 
+                  customers { 
+                    edges { 
+                      node {
+                        id
+                        name
+                        firstName
+                        lastName
+                      } 
+                    } 
+                  }
+                } 
+              }`
+            })
+          );
+}
+
+function transformData(item, index) {
+  var customers = {
+    id: item.node.id, 
+    name: item.node.name, 
+    firstName: item.node.firstName,
+    lastName: item.node.lastName
+  };
+ 
+  return customers;
+}
+
+function useData(data) {
+  var i;
+  for(i = 0; i < data.length; i++) {
+    document.getElementById("demo").innerHTML += data[i].id;
+  }
+}
+
+
+
 // fetch("https://swapi.co/api/people/1/", {
 //     method: 'get'
 //   })
@@ -22,30 +79,34 @@
   
   
   
-  
-fetch('https://gql.waveapps.com/graphql/public', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Authorization': 'Bearer 8NBKTN3t9pfYUS0GsEoNapjY6ooRbi'
-  },
-  body: JSON.stringify({  query: `{ 
-                            businesses { 
-                              edges { 
-                                node { 
-                                  id 
-                                  name 
-                                } 
-                              }
-                            } 
-                          }`
-                        })
-  })
-  .then(r => r.json())
-  .then(function(data) {
-    //data => console.log('data returned:', data)
-    var theData = data;
-    console.log(theData);
-    console.log(data.businesses.edges[""0""].node.name);
-  });
+// fetch('https://gql.waveapps.com/graphql/public', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Accept': 'application/json',
+//     'Authorization': 'Bearer 8NBKTN3t9pfYUS0GsEoNapjY6ooRbi'
+//   },
+//   body: JSON.stringify({  query: `{ 
+//                             business(id: "QnVzaW5lc3M6NjNiOTVkZGItNWRkOS00MzI0LWEzNGYtMDkxOTJmNjNjNDc0") { 
+//                               customers { 
+//                                 edges { 
+//                                   node {
+//                                     id
+//                                     name
+//                                     firstName
+//                                     lastName
+//                                   } 
+//                                 } 
+//                               }
+//                             } 
+//                           }`
+//                         })
+//   })
+//   .then(r => r.json())
+//   .then(function(data) {
+//   // data => console.log(data.business.customers.map(d => d.node));
+// //   var theData = data.business.customers.edges["0"].node.name;
+//     console.log(data);
+//     console.log(data.business.customers.edges["0"].node.name);
+//     //console.log(item.name);
+//   });
